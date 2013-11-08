aad33327c6ed318417d5ba947ff3283dcdf00f0f
Fabiano Morais <fabiano@MacBook-Pro-de-Fabiano.local>
2013-11-08 21:23:00 -0200

    Iniciando o projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wele::Application.config.secret_key_base = '1055563c8d712e6adadc01d833cff1994fd42c76151f94e320deb9d536135ac5a44ccdd79a30ffe96363f717d3aadd3a6a1636b958ed4c6a64519878a4949804'
